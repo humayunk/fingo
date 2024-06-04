acdcfa7751bd6ebef08c2799a9bdaa29d22a34ea
Juliette Hutton <juliettehutton2@gmail.com>
2024-06-04 14:38:03 -0400

    added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :xp, presence: true, numericality: { only_integer: true }
+  validates :streak, presence: true, numericality: { only_integer: true }
 end
