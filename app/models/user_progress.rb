fff6bcb9c09eb086e9cc3af7037d5e5a70619daa
Juliette Hutton <juliettehutton2@gmail.com>
2024-06-04 14:50:06 -0400

    added validation in user_progress model

--- app/models/user_progress.rb
+++ app/models/user_progress.rb
@@ -1,4 +1,10 @@
 class UserProgress < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :lesson_id, presence: true, numericality: { only_integer: true }
+  validates :completed, presence: true, inclusion: { in: [true, false] }
+  validates :score, presence: true, numericality: { only_integer: true }
+  validates :current_step, presence: true, numericality: { only_integer: true }
 end
