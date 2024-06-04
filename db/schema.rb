7a825109340686157d75f0813c29e2f60b09ec4f
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-06-04 15:22:30 -0400

    associations/validations created for enrollment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_163803) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_185746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_163803) do
     t.boolean "is_correct"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "step_id", null: false
+    t.index ["step_id"], name: "index_answers_on_step_id"
   end
 
   create_table "courses", force: :cascade do |t|
@@ -33,6 +35,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_163803) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -79,6 +85,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_163803) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "steps"
+  add_foreign_key "enrollments", "courses"
+  add_foreign_key "enrollments", "users"
   add_foreign_key "user_progresses", "lessons"
   add_foreign_key "user_progresses", "users"
 end
