ae70970ee055e4690ee28bd11d8b861c4ef8b85a
jeff prince <jeffryaime@gmail.com>
2024-06-04 14:48:34 -0400

    lessons associations and validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
+  belongs_to :course
+  has_many :steps, dependent: :destroy
+
+  # Validations
+  validates :title, presence: true, uniqueness: { scope: :course_id }
+  validates :content, presence: true
+
   def to_param
     title
   end
