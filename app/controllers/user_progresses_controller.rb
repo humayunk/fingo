c212658cdfecc9f436f272299708b5c9ca421981
humayun khan <humayun.n.k@gmail.com>
2024-06-07 12:03:06 -0400

    reverted user controller and courses show page

--- app/controllers/user_progresses_controller.rb
+++ app/controllers/user_progresses_controller.rb
@@ -1,59 +1,56 @@
 class UserProgressesController < ApplicationController
   before_action :set_lesson, only: [:create]
   before_action :set_user_progress, only: [:update, :complete]
-  before_action :set_current_user, only: [:create]
 
   def create
     @user_progress = UserProgress.new(user: current_user, lesson: @lesson, completed: false, score: 0, current_step: 1)
-
+    # @user_progress.save
+    # TODO: CODE redirect. Where do we want to redirect after creating user_progress?
+    # juliette added this
     if @user_progress.save
-      add_coins(10)
-      redirect_to lesson_path(@lesson.title, step: 1)
+      redirect_to lesson_path(@lesson.title)
     else
       redirect_to course_path(@lesson.course.title), alert: "Could not start lesson."
     end
   end
 
   def update
-    @enrollment = Enrollment.where(course: @user_progress.lesson.course, user: current_user)
-    @lesson_list = @user_progress.lesson.course.lessons
-    @user_progress_list = UserProgress.where(lesson: @lesson_list.pluck(:id), user: current_user)
     @user_progress.current_step = params[:step]
 
-    if @user_progress.current_step == @user_progress.lesson.steps.count
-      add_coins(50)
-      @user_progress.update(completed: true)
-      if @lesson_list.count == @user_progress_list.count
-        @enrollment.update(completed: true)
-        add_coins(100)
-        redirect_to courses_path, notice: 'Course was successfully completed!'
-      else
-        redirect_to lesson_path(@user_progress.lesson), notice: 'Lesson was successfully completed!'
-      end
+    if @user_progress.save
+      redirect_to lesson_path(@user_progress.lesson.title, step: params[:step])
     else
-      if @user_progress.save
-        redirect_to lesson_path(@user_progress.lesson, step: @user_progress.current_step)
-      else
-        redirect_to lesson_path(@user_progress.lesson), alert: "Could not update progress."
-      end
+      redirect_to lesson_path(@user_progress.lesson.title), alert: "Could not update progress."
+    end
+
+    # @user_progress.increment(:score)
+
+    # TODO: more logic required for validating answers later on in project.
+    # if step is just content, move to next step. If it is a question, will have to validate answer
+    # before moving to next step
+  end
+
+  def complete
+    if @user_progress.current_step == @user_progress.lesson.steps.count
+      @user_progress.update(completed: true)
+      redirect_to celebration_lesson_path(@user_progress.lesson)
+    else
+      redirect_to lesson_path(@user_progress.lesson.title), notice: "Please complete the steps."
     end
   end
 
   private
 
   def set_lesson
-    @lesson = Lesson.find(params[:lesson_id])
+    @lesson = Lesson.find_by!(title: params[:lesson_title])
   end
+
+  # Strong params(below) not presently required. Left code here in case require for future.
+  # def user_progress_params
+  #   params.require(:user_progress).permit(:user_id, :lesson_id, :completed, :score, :current_step)
+  # end
 
   def set_user_progress
     @user_progress = UserProgress.find(params[:id])
-  end
-
-  def set_current_user
-    @current_user = current_user
-  end
-
-  def add_coins(amount)
-    @current_user.increment!(:coins, amount)
   end
 end
