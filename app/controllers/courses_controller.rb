b7944cae5f5d56ec8c91b78ff7fec2b17e601de9
humayun khan <humayun.n.k@gmail.com>
2024-06-07 16:01:46 -0400

    added logic for lessons being locked and unlocked

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,5 +8,7 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find_by(title: params[:title])
     return redirect_to courses_path, alert: "Course not found." unless @course
+
+    @enrollment = @course.enrollments.active_for(current_user)
   end
 end
