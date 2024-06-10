00cd20ad49d670d5d09110ddbd4b22c3666c86e1
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-06-10 12:06:40 -0400

    coins feature works

--- db/schema.rb
+++ db/schema.rb
@@ -14,34 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "answers", force: :cascade do |t|
     t.text "content"
     t.boolean "is_correct"
@@ -56,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
   create_table "enrollments", force: :cascade do |t|
@@ -76,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "course_id", null: false
+    t.string "image_name"
     t.integer "order_rank"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
@@ -88,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
     t.datetime "updated_at", null: false
     t.bigint "lesson_id", null: false
     t.string "title"
+    t.string "image_name"
     t.index ["lesson_id"], name: "index_steps_on_lesson_id"
   end
 
@@ -116,14 +91,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
     t.string "last_name"
     t.string "nickname"
     t.integer "coins"
-    t.integer "streak", default: 0
-    t.datetime "last_completed_at"
+    t.integer "streak"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "steps"
   add_foreign_key "enrollments", "courses"
   add_foreign_key "enrollments", "users"
