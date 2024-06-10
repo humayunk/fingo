45d8fef0cd4ee369b344b4204e2bfa18d7b7f80e
Juliette Hutton <juliettehutton2@gmail.com>
2024-06-10 17:27:14 -0400

    solved conflicts pt.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_170635) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_170635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "course_id", null: false
-    t.string "image_name"
     t.integer "order_rank"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
