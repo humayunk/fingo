d56fe15f22923552f94ce31a55053735c5931004
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-06-10 17:32:06 -0400

    user can enroll in multiple courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,15 @@ class User < ApplicationRecord
   # validates :streak, presence: true, numericality: { only_integer: true }
 
   has_many :user_progresses
+
   has_many :enrollments
-  has_one :active_enrollment, -> { where completed: false}, class_name: 'Enrollment', foreign_key: :user_id
-  has_one :active_course, through: :active_enrollment, class_name: "Course", source: :course
+  has_many :in_progress_enrollments, -> { where completed: false }, class_name: 'Enrollment', foreign_key: :user_id
+  has_many :completed_enrollments, -> { where completed: true }, class_name: 'Enrollment', foreign_key: :user_id
+
+  has_many :courses, through: :enrollments
+  has_many :in_progress_courses, through: :in_progress_enrollments, class_name: "Course", source: :course
+  has_many :completed_courses, through: :completed_enrollments, class_name: "Course", source: :course
+
 
 
   # if in future, can enroll in multiple courses can change to has_many: active_enrollments
