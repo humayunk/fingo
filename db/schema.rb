5c1017e949332d1f60a80689f50c4b10d12ed4a5
jeff prince <jeffryaime@gmail.com>
2024-06-11 14:47:29 -0400

    onboarding column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_184514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_185940) do
     t.integer "coins"
     t.integer "streak", default: 0
     t.datetime "last_completed_at"
+    t.jsonb "onboarding_data"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
